9ae487cc50ead93cc88eab4edf22e7f7699ddf52
George Chen <georgechen1338@gmail.com>
2020-03-19 18:20:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -61,16 +61,16 @@ def monopoly_with_fourth_tier
       :names => {
         :reading_railroad => {
           :mortgage_value => 100
-        }
-        :pennsylvania_railroad =>{
+        },
+        :pennsylvania_railroad => {
           :mortgage_value => 200
-        }
+        },
         :b_and_o_railroad => {
           :mortgage_value => 400
-        }
+        },
         :shortline_railroad => {
           :mortgage_value => 800
-        }
+        },
       }
       }
     }
